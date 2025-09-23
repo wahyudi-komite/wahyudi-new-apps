5e0827f12901e432a34944b8f4cafb347e6b17c5
unknown <adhye.yudhie@gmail.com>
2025-09-23 17:51:40 +0700

    Typeorm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = parseInt(process.env.DATABASE_PORT ?? '3010', 10);
+  app.setGlobalPrefix('api-new-apps/v1/');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.ENV_CORS,
+    credentials: true,
+  });
+
+  await app.listen(port, '0.0.0.0');
+  console.log(process.env.DATABASE_HOST);
+  console.log(port);
 }
 bootstrap();
